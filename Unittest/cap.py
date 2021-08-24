b2ba0d5657583ce0621ab070ed2ce9178edb45e3
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-08-24 20:13:20 -0300

    Unittest ERROR Example

---
+++ Unittest/cap.py
@@ -0,0 +1,15 @@
+def cap_text(text):
+    '''
+    Input a string
+    Output the capitalized string
+    '''
+    return text.capitalize()
+
+
+def cap2_text(text):
+    '''
+    Input a string
+    Output the capitalized string
+    '''
+    return text.title()
+
--- pylint_overview/simple1.py
+++ pylint_overview/simple1.py
@@ -1,4 +1,13 @@
-a = 1
-b = 2
-print(a)
-print(B)
+'''
+A very simple function
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+myfunc()
